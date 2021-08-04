a3affb4cf0476e626f00387d1ebeee0f8b51c732
Olivia Okafor <oliviaokafor@Olivias-Air.attlocal.net>
2021-08-03 23:39:18 -0400

    updating files

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,54 @@
+SELECT employees.emp_no, 
+		employees.first_name, 
+		employees.last_name,
+		titles.title,
+		titles.from_date,
+		titles.to_date
+INTO retirement_titles
+FROM employees
+	INNER JOIN titles 
+	ON (employees.emp_no = titles.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY employees.emp_no ;
+
+SELECT * FROM retirement_titles;
+
+DROP TABLE retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no, retirement_titles.first_name, retirement_titles.last_name, retirement_titles.title
+INTO uniqretirement_titles
+FROM retirement_titles
+ORDER BY retirement_titles.emp_no, retirement_titles.to_date DESC;
+
+SELECT * FROM uniqretirement_titles;
+
+
+DROP TABLE retiring_titles;
+
+
+SELECT COUNT(uniqretirement_titles.title), 
+uniqretirement_titles.title
+INTO retiring_titles
+FROM uniqretirement_titles
+GROUP BY uniqretirement_titles.title
+ORDER BY COUNT(uniqretirement_titles.title) DESC;
+
+SELECT * FROM retiring_titles;
+
+
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,employees.first_name,
+		employees.last_name, 
+		employees.birth_date,
+		dept_emp.from_date,
+		dept_emp.to_date, 
+		titles.title
+INTO mentor_elig
+FROM employees	
+	INNER JOIN dept_emp
+	ON (employees.emp_no = dept_emp.emp_no)
+	INNER JOIN titles
+	ON (employees.emp_no = titles.emp_no)
+WHERE (dept_emp.to_date = '9999-01-01')
+	AND (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no ;
