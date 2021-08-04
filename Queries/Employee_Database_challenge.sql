638f8e6e1a25599caced825e87e8652bb7171a1d
Olivia Okafor <oliviaokafor@Olivias-Air.attlocal.net>
2021-08-04 18:45:16 -0400

    Updating the data and queries folders

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,5 @@
+--Creating a table with the retiring employees by titles 
+
 SELECT employees.emp_no, 
 		employees.first_name, 
 		employees.last_name,
@@ -27,6 +29,8 @@ SELECT * FROM uniqretirement_titles;
 DROP TABLE retiring_titles;
 
 
+-- Counting the unique retirement titles of the retiring employees and counting them.
+
 SELECT COUNT(uniqretirement_titles.title), 
 uniqretirement_titles.title
 INTO retiring_titles
@@ -36,6 +40,8 @@ ORDER BY COUNT(uniqretirement_titles.title) DESC;
 
 SELECT * FROM retiring_titles;
 
+
+--Creating the mentor eligible employees table 
 
 SELECT DISTINCT ON (employees.emp_no) employees.emp_no,employees.first_name,
 		employees.last_name, 
@@ -52,3 +58,75 @@ FROM employees
 WHERE (dept_emp.to_date = '9999-01-01')
 	AND (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY employees.emp_no ;
+
+SELECT * FROM mentor_elig;
+
+
+
+-- Additional queries for new tables for my summary section of deliverable 3
+
+SELECT COUNT(mentor_elig.title), 
+mentor_elig.title
+INTO mentor_titles
+FROM mentor_elig
+GROUP BY mentor_elig.title
+ORDER BY COUNT(mentor_elig.title) DESC;
+
+SELECT * FROM mentor_titles;
+
+
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,employees.first_name,
+		employees.last_name, 
+		employees.birth_date,
+		dept_emp.from_date,
+		dept_emp.to_date, 
+		dept_emp.dept_no,
+		titles.title
+INTO mentor_elig1
+FROM employees	
+	INNER JOIN dept_emp
+	ON (employees.emp_no = dept_emp.emp_no)
+	INNER JOIN titles
+	ON (employees.emp_no = titles.emp_no)
+WHERE (dept_emp.to_date = '9999-01-01')
+	AND (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no ;
+
+SELECT * FROM mentor_elig1;
+
+SELECT COUNT(mentor_elig1.dept_no), 
+mentor_elig1.dept_no
+INTO mentor_dept
+FROM mentor_elig1
+GROUP BY mentor_elig1.dept_no
+ORDER BY COUNT(mentor_elig1.dept_no) DESC;
+
+SELECT * FROM mentor_dept;
+
+
+SELECT employees.emp_no, 
+		employees.first_name, 
+		employees.last_name,
+		titles.title,
+		titles.from_date,
+		titles.to_date, 
+		dept_emp.dept_no
+INTO retirement_departments
+FROM employees
+	INNER JOIN titles 
+	ON (employees.emp_no = titles.emp_no)
+	INNER JOIN dept_emp
+	ON (employees.emp_no = dept_emp.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY employees.emp_no ;
+
+SELECT * FROM retirement_departments;
+
+SELECT COUNT(retirement_departments.dept_no), 
+retirement_departments.dept_no
+INTO retiring_dept
+FROM retirement_departments
+GROUP BY retirement_departments.dept_no
+ORDER BY COUNT(retirement_departments.dept_no) DESC;
+
+SELECT * FROM retiring_dept;
